996207c1fe2a917f456722eb7a38f6851dc4d2b3
Peter Polidoro <peter@polidoro.io>
2021-09-17 10:58:53 -0400

    Remove spurious comments

--- src/FieldStimulationController/FieldStimulationController.cpp
+++ src/FieldStimulationController/FieldStimulationController.cpp
@@ -145,24 +145,6 @@ void FieldStimulationController::stimulationPwm(long delay,
   event_controller_.enable(pwm_event_id_pair_);
 }
 
-// Handlers must be non-blocking (avoid 'delay')
-//
-// modular_server_.parameter(parameter_name).getValue(value) value type must be either:
-// fixed-point number (int, long, etc.)
-// floating-point number (float, double)
-// bool
-// const char *
-// ArduinoJson::JsonArray
-// ArduinoJson::JsonObject
-// const ConstantString *
-//
-// For more info read about ArduinoJson parsing https://github.com/janelia-arduino/ArduinoJson
-//
-// modular_server_.property(property_name).getValue(value) value type must match the property default type
-// modular_server_.property(property_name).setValue(value) value type must match the property default type
-// modular_server_.property(property_name).getElementValue(element_index,value) value type must match the property array element default type
-// modular_server_.property(property_name).setElementValue(element_index,value) value type must match the property array element default type
-
 void FieldStimulationController::stimulationOnEventHandler(int arg)
 {
   bool inverted;
